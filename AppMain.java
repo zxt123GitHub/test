dcd2fc082dc6f752e2a2b9ebfeb085decb1a38e9
zxt123GitHub! <1967576791@qq.com>
2022-04-18 23:46:42 +0800

    test

---
+++ AppMain.java
@@ -0,0 +1,14 @@
+
+public class AppMain {
+		public static void main(String[] args) 
+		Doll beibei=new Doll("±´±´");
+		Doll jingjing=new Doll("¾§¾§");
+		Doll huanhuan=new Doll("»¶»¶");
+		Doll yingying=new Doll("Ó­Ó­");
+		Doll nini=new Doll("ÄÝÄÝ");
+		beibei.speakO;
+		jingjing.speak0;
+		huanhuan.speak0;
+		yingy ing.speak(; nini.speak0;
+
+}
